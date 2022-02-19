ce9b16b6e5d9cc4dee836c706e3b6231ff4504f7
poix3 <scu_wong@outlook.com>
2022-02-19 22:25:40 +0100

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,30 @@
-# 1.1 
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread, imshow
+from skimage.color import rgb2hsv, hsv2rgb
+from skimage.util import img_as_float
+
+def gamma_transform(image, gamma, c = 1):
+    image = img_as_float(image)
+    return np.array(c*image**gamma)
+
+
+# 1.1
+img = imread('lena.png', as_gray=True)
+# imshow(gamma_transform(img, 0.3)) # gamma = [0.3, 0.5, 1.0, 2.0, 3.0]
+
+# 1.2
+autumn = imread('autumn.tif')
+# imshow(autumn)
+# imshow(gamma_transform(autumn, 0.3)) # gamma = [0.3, 0.5, 1.0, 2.0, 3.0]
+
+# 1.3
+# imshow(autumn)
+hsv_img = rgb2hsv(autumn)
+#imshow(hsv_img)
+value_img = hsv_img[:, :, 2]
+# imshow(value_img)
+value_img_gamma = gamma_transform(value_img, 3.0) # gamma = [0.3, 0.5, 0.8, 2.0, 3.0]
+hsv_img[:, :, 2] = value_img_gamma
+# imshow(hsv_img)
+# imshow(hsv2rgb(hsv_img))
